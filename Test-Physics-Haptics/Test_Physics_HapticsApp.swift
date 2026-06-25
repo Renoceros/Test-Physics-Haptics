12575e46e6a6816bbbf1bafbd2fd5c1fc16e88bf
Renoceros <130336017+Renoceros@users.noreply.github.com>
2026-06-25 10:30:22 +0800

    Initial Commit

---
+++ Test-Physics-Haptics/Test_Physics_HapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Physics_HapticsApp.swift
+//  Test-Physics-Haptics
+//
+//  Created by Moreno Kristovan on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Test_Physics_HapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
